133e836a685551157aeebb428b13165cbca31bee
Krzysztof Modras <krzysztof.modras@gmail.com>
2013-09-26 22:43:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vectorsplay::Application.config.secret_token = '36768dd22903e76d36aa73d56e4907cfc3c187925971ac58a459495f0cddc73bebac660b564bc60a540305aaa712abf20e2cdc261bb5f2d26f7154ef430fcf4d'
